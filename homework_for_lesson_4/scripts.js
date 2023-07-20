4cd59826660a77ea73fc311bfc0042c3252ef827
Galiulin.Nik <galiulin.nik@gmail.com>
2023-07-20 22:52:58 +0600

    homework_lesson_4

--- homework_for_lesson_4/scripts.js
+++ homework_for_lesson_4/scripts.js
@@ -15,24 +15,22 @@ fetch(API_URL_GEO_DATA)
   .then(function (data) {
     let strCoordinates =
       data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos; //получили координаты нашего города
-    let coordinates = strCoordinates.split(" "); //разбили строку на массив строк используя разделитель пробел
+    console.log(strCoordinates)
+      let coordinates = strCoordinates.split(" "); //разбили строку на массив строк используя разделитель пробел
     const API_OPEN_METEO = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${coordinates[0]}&longitude=${coordinates[1]}&hourly=pm10,pm2_5`;
     fetch(API_OPEN_METEO)
       .then((resp) => resp.json())
       .then(function (data) {
         //время : количество частиц pm10 : количество частиц pm2_5
-        let arr = [];
-        arr = data.hourly.time;
-        console.log(arr.lenght);
-        // for (let i = 0; i < 119; i++) {
-        //   let ol = createNode("ol");
-        //   let hr = createNode("hr");
-        //   let p = createNode("p");
-        //   p.innerHTML = `Дата и время: ${data.hourly.time[i]} <br> pm10: ${data.hourly.pm10[i]} <br> pm2_5: ${data.hourly.pm2_5[i]}`;
-        //   append(ol, p);
-        //   append(ol, hr);
-        //   append(div, ol);
-        // }
+        for (let i = 0; i < data.hourly.time.length; i++) {
+          let ol = createNode("ol");
+          let hr = createNode("hr");
+          let p = createNode("p");
+          p.innerHTML = `Дата и время: ${data.hourly.time[i]} <br> pm10: ${data.hourly.pm10[i]} <br> pm2_5: ${data.hourly.pm2_5[i]}`;
+          append(ol, p);
+          append(ol, hr);
+          append(div, ol);
+        }
       })
       .catch(function (error) {
         console.log(error);
