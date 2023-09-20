95b180ca0e67b475708a7eccd983f28b88fb8e5f
Galiulin.Nik <Galiulin.nik@gmail.com>
2023-09-20 16:59:49 +0600

    homework_for_lesson_22

---
+++ homework_for_lesson_22/src/reducers/taskReducer.js
@@ -0,0 +1,35 @@
+const initialState = {
+  tasks: [
+    {
+      id: "1",
+      text: "test",
+      status: "active",
+    },
+  ],
+};
+
+export const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TASK": {
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    }
+    case "DELETE_TASK": {
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
+    }
+    // case 'UPDATE_TASK':{
+    //     return {
+    //         ...state,
+    //         tasks: state.tasks.filter(task => task.id === action.payload)
+    //     }
+    // }
+    default:
+      return state;
+  }
+};
+export default taskReducer;
