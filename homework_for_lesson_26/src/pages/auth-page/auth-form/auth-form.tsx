50bede2726998d5c133d11b47c40e82e703b5d84
Galiulin.Nik <galiulin.nik@gmail.com>
2023-10-12 23:31:01 +0600

    homework26

---
+++ homework_for_lesson_26/src/pages/auth-page/auth-form/auth-form.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Typography from "@mui/material/Typography";
+
+export const AuthForm = () => {
+  return (
+    <div className="auth-form">
+      <Typography variant="h4" component="div">
+        h4. Heading
+      </Typography>
+    </div>
+  );
+};
---
+++ homework_for_lesson_26/src/pages/auth-page/auth-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import { AuthForm } from "./auth-form";
+
+export const AuthPage: React.FC = () => {
+  return (
+    <div className="auth-page">
+      <img src="" alt="" />
+      <AuthForm />
+    </div>
+  );
+};
