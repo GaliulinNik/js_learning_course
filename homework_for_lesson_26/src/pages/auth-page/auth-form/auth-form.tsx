ec6311e174aa2a54f8f4c83cfcda99f0446f456d
Galiulin.Nik <galiulin.nik@gmail.com>
2023-10-13 02:02:25 +0600

    lesson26 13/10/2023

--- homework_for_lesson_26/src/pages/auth-page/auth-form/auth-form.tsx
+++ homework_for_lesson_26/src/pages/auth-page/auth-form/auth-form.tsx
@@ -1,13 +1,67 @@
 import React from "react";
-import "./style.css";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import "./style.css";
+
+interface ISignForm {
+  login: string;
+  password: string;
+}
 
 export const AuthForm = () => {
+  const { handleSubmit, control } = useForm<ISignForm>();
+  const onSubmit: SubmitHandler<ISignForm> = (data) => console.log(data);
   return (
     <div className="auth-form">
       <Typography variant="h4" component="div">
-        h4. Heading
+        Вход
       </Typography>
+      <Typography variant="subtitle1" component="div" gutterBottom={true} className="subtitle">
+        введите логин и пароль, чтобы получить доступ
+      </Typography>
+      <form className="formlogin" onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="login"
+          render={({ field }) => (
+            <TextField
+              label="Логин"
+              placeholder="Введите имя пользователя"
+              margin="normal"
+              fullWidth={true}
+              onChange={(event) => field.onChange(event)}
+              value={field.value}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <TextField
+              label="Пароль"
+              type="password"
+              placeholder="Введите пароль"
+              margin="normal"
+              fullWidth={true}
+              onChange={(event) => field.onChange(event)}
+              value={field.value}
+            />
+          )}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          fullWidth={true}
+          sx={{ marginTop: 2 }}
+        >
+          Войти
+        </Button>
+      </form>
     </div>
   );
 };
--- homework_for_lesson_26/src/pages/auth-page/auth-page.tsx
+++ homework_for_lesson_26/src/pages/auth-page/auth-page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles.css";
 import { AuthForm } from "./auth-form";
+import logo from "../../logo-75-wh.png";
 
 export const AuthPage: React.FC = () => {
   return (
     <div className="auth-page">
-      <img src="" alt="" />
       <AuthForm />
+      <img src={logo} alt="image" />
     </div>
   );
 };
