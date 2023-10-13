fd5c50d3308663d1ea1bc7ce42e44f6c32dcdad7
Galiulin.Nik <Galiulin.nik@gmail.com>
2023-10-13 11:04:12 +0600

    end homework_26

--- homework_for_lesson_26/src/pages/auth-page/auth-form/auth-form.tsx
+++ homework_for_lesson_26/src/pages/auth-page/auth-form/auth-form.tsx
@@ -3,7 +3,12 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import {
+  useForm,
+  Controller,
+  SubmitHandler,
+  useFormState,
+} from "react-hook-form";
 import "./style.css";
 
 interface ISignForm {
@@ -13,19 +18,35 @@ interface ISignForm {
 
 export const AuthForm = () => {
   const { handleSubmit, control } = useForm<ISignForm>();
+  const { errors } = useFormState({ control });
+
   const onSubmit: SubmitHandler<ISignForm> = (data) => console.log(data);
   return (
     <div className="auth-form">
       <Typography variant="h4" component="div">
         Вход
       </Typography>
-      <Typography variant="subtitle1" component="div" gutterBottom={true} className="subtitle">
+      <Typography
+        variant="subtitle1"
+        component="div"
+        gutterBottom={true}
+        className="subtitle"
+      >
         введите логин и пароль, чтобы получить доступ
       </Typography>
       <form className="formlogin" onSubmit={handleSubmit(onSubmit)}>
         <Controller
           control={control}
           name="login"
+          rules={{
+            required: "Не указано имя",
+            validate: (value: string) => {
+              if (!value.match(/^(?=.*[a-zA-Z0-9]).{5,20}$/)) {
+                return "Некорректное имя [a-z, A-Z, 0-9 от 5 до 20]";
+              }
+              return true;
+            },
+          }}
           render={({ field }) => (
             <TextField
               label="Логин"
@@ -34,12 +55,27 @@ export const AuthForm = () => {
               fullWidth={true}
               onChange={(event) => field.onChange(event)}
               value={field.value}
+              error={!!errors.login?.message}
+              helperText={errors.login?.message}
             />
           )}
         />
         <Controller
           control={control}
           name="password"
+          rules={{
+            required: "Не указан пароль",
+            validate: (value: string) => {
+              if (
+                !value.match(
+                  /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[a-z]).*$/
+                )
+              ) {
+                return "Некорректный пароль [a-z, 0-9, спецсимволы - минимум 8]";
+              }
+              return true;
+            },
+          }}
           render={({ field }) => (
             <TextField
               label="Пароль"
@@ -49,6 +85,8 @@ export const AuthForm = () => {
               fullWidth={true}
               onChange={(event) => field.onChange(event)}
               value={field.value}
+              error={!!errors.password?.message}
+              helperText={errors.password?.message}
             />
           )}
         />
